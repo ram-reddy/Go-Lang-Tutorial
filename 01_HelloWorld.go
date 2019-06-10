c65ad2b23b0ce1fa447efab863257460d97affe6
RamiReddy Palamakula <ramireddy.palamakula@paysafe.com>
2019-06-10 15:40:48 +0530

    [001] Hello World!

---
+++ 01_HelloWorld.go
@@ -0,0 +1,7 @@
+package main //package declaration
+
+import "fmt" //importing format package
+
+func main() { //main function declaration
+    fmt.Println("Hello World!") //calling Println public method with string argument 
+}
